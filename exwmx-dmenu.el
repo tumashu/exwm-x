f1e5ccd5792ad46df55c09012bde321ae1451f06
Feng Shu <tumashu@163.com>
2017-04-26 06:30:44 +0800

    exwmx-dmenu: save more history

--- exwmx-dmenu.el
+++ exwmx-dmenu.el
@@ -103,20 +103,25 @@ dmenu should keep a record. "
                                 (or (string-match-p "^\\." x)
                                     (string-match-p "^ *$" x)))
                             (cl-remove-duplicates
-                             (append exwmx-dmenu--history
-                                     (exwmx-dmenu--get-emacs-commands)
-                                     exwmx-dmenu--commands)
+                             (append
+                              (let* ((history (cl-remove-duplicates
+                                               exwmx-dmenu--history
+                                               :from-end t :test #'equal))
+                                     (length (- (length history) 1)))
+                                (cl-subseq history 0 (min length exwmx-dmenu-history-size)))
+                              (exwmx-dmenu--get-emacs-commands)
+                              exwmx-dmenu--commands)
                              :from-end t :test #'equal))
               :keymap exwmx-dmenu-ivy-minibuffer-map)))))
-    (setq exwmx-dmenu--history
-          (cons command
-                (remove command exwmx-dmenu--history)))
-    (when (> (length exwmx-dmenu--history)
-             exwmx-dmenu-history-size)
-      (setcdr (nthcdr (- exwmx-dmenu-history-size 1)
-                      exwmx-dmenu--history)
-              nil))
-    (exwmx-dmenu--run command)))
+    (if (< (length command) 1)
+        (message "Exwm-X: No command is inputed.")
+      (setq exwmx-dmenu--history
+            (cons command exwmx-dmenu--history))
+      ;; We must record more history to cache file.
+      (when (> (length exwmx-dmenu--history) 101)
+        (setq exwmx-dmenu--history
+              (cl-subseq exwmx-dmenu--history 0 exwmx-dmenu-history-size)))
+      (exwmx-dmenu--run command))))
 
 (defun exwmx-dmenu--run-with-terminal (command)
   (let ((cmd (format "%s -e 'bash -c %S'"
