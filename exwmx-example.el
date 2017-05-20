0885fdfed3107cb418f8170bbc577567dedd2f86
Feng Shu <tumashu@163.com>
2017-05-20 22:12:07 +0800

    C-t C-r -> exwmx-appconfig

--- exwmx-example.el
+++ exwmx-example.el
@@ -98,6 +98,7 @@
 ;;
 (exwm-input-set-key (kbd "C-t ;") 'exwmx-dmenu)
 (exwm-input-set-key (kbd "C-t C-e") 'exwmx-sendstring)
+(exwm-input-set-key (kbd "C-t C-r") 'exwmx-appconfig)
 
 (exwm-input-set-key (kbd "C-t 1")  'exwmx-switch-to-1-workspace)
 (exwm-input-set-key (kbd "C-t 2")  'exwmx-switch-to-2-workspace)
