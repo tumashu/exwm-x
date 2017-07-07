9e033274f1db04f0705a693ea8ce0ed34fbebd59
Feng Shu <tumashu@163.com>
2017-07-07 18:22:47 +0800

    Add exwmx-floating--move-to-position

--- exwmx-floating.el
+++ exwmx-floating.el
@@ -56,6 +56,43 @@ FIXME: This is a hack, it should be improved in the future."
   (advice-add 'exwm-input--update-focus
               :before #'exwmx-floating--smart-hide))
 
+
+(defun exwmx-floating--move-to-position (x y)
+  "Move current floating window to position: `x', `y'."
+  (when (and (>= x 0)
+             (>= y 0)
+             (eq major-mode 'exwm-mode)
+             exwm--floating-frame)
+    (let* ((edges (window-inside-absolute-pixel-edges)))
+      (xcb:+request exwm--connection
+          (make-instance 'xcb:ConfigureWindow
+                         :window exwm--container
+                         :value-mask (eval-when-compile
+                                       (logior xcb:ConfigWindow:X
+                                               xcb:ConfigWindow:Y))
+                         :x x
+                         :y y))
+      ;; Inform the X window that its absolute position is changed
+      (xcb:+request exwm--connection
+          (make-instance 'xcb:SendEvent
+                         :propagate 0 :destination exwm--id
+                         :event-mask xcb:EventMask:StructureNotify
+                         :event (xcb:marshal
+                                 (make-instance 'xcb:ConfigureNotify
+                                                :event exwm--id
+                                                :window exwm--id
+                                                :above-sibling xcb:Window:None
+                                                :width (- (elt edges 2)
+                                                          (elt edges 0))
+                                                :height (- (elt edges 3)
+                                                           (elt edges 1))
+                                                :border-width 0
+                                                :override-redirect 0
+                                                :x x
+                                                :y y)
+                                 exwm--connection))))
+    (xcb:flush exwm--connection)))
+
 (defun exwmx-floating-mouse-move (start-event)
   "This is a mouse drag event function used by exwmx-button,
 when drag mouse from such button, move current floating window dynamic."
